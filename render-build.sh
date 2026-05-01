6395a0772cb3fcc61750e54186c699c111b8d4be
MIVS <admin@appli-suivi-projets.com>
2026-05-01 13:02:10 +0000

    Force Python 3.11.9 and use Pillow 10.2.0 wheel

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install system dependencies for Pillow
-apt-get update
-apt-get install -y libjpeg-dev zlib1g-dev
-
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
